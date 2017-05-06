8c1dee87c29cb2e7bbaccebba6056052c4808355
JC <jcsmileyjr@gmail.com>
2017-05-06 07:25:26 -0500

    Update user's weight
    Creates a weightSubmit function used on the weighIn page. Searches
    through the users array of objects for a name match. If there is a name
    match, the weight parameter is subtracted from the startWeight to create
    a new weightLoss for the matched array element. The getWeightLoss global
    method is called to transform updated users array into the myArray
    variable (everything without the players weight and sent to the
    controller to update the Player Ranking Page).

--- 10MON.js
+++ 10MON.js
@@ -8,11 +8,16 @@ app.controller('monsterController', function($scope, $http, $filter){
     $scope.newArray = []; /*array populated with each userdata array element except the weight*/
     
     /* The userData array is placed into the newArray variable without the players weight */
-    for(var j=0; j<$scope.userData.length;j++){
-        /*myArray variable is populated with each user array element except the weight*/
-        $scope.newArray[j]= {"name":$scope.userData[j].name, "weightLoss":$scope.userData[j].weightLoss};  
-    }/*end of j-loop*/     
+    $scope.getWeightLoss = function(){
+        /*loops throught the userData array*/
+        for(var j=0; j<$scope.userData.length;j++){
+            /*myArray variable is populated with each user array element except the weight*/
+            $scope.newArray[j]= {"name":$scope.userData[j].name, "weightLoss":$scope.userData[j].weightLoss};  
+        }/*end of j-loop*/     
+    }
     
+    /*inital setting of app's information used the Ranking Page */
+    $scope.getWeightLoss();
 
     /*add code for disabling nav if logIn do not return true*/
     $scope.setView = function(view){
@@ -22,22 +27,36 @@ app.controller('monsterController', function($scope, $http, $filter){
     /*the starting page is set to the login page */
     $scope.setView('logInPage');
     
-    $scope.name = "Mary Runner";
-    $scope.newWeight = function(name){
-        for(var i=0;i<$scope.userData.length;i++){
-            if($scope.userData[i].name===name){
-                $scope.foundUser = $scope.userData[i];
-                break;    
-            }/* End of if statement*/
-        }/*end of for loop*/
-    }/*end of newWeight function*/
+    /*retrieves the log-in page userName and create a global varible to track user.*/
+    $scope.currentUser = "";
     
-    $scope.newWeight($scope.name);
+    /*                                        Weight-In Codebase               */  
     
+    /*function attached to weighIn page submit button. Takes as parameters the current user input weigh variable (from inputWeighIn text-box) and currentUser global varibile from logIn. */
+    $scope.weighIn = function(weight) {
+        
+        /*This for loop searches through the users array of objects for a name match. If there is a name match, the weight parameter is subtracted from the startWeight to create a new weightLoss for the matched array element. Using teh getWeightLoss method, the updated users array is placed into the myArray variable without the players weight and sent to the controller to update the Player Ranking Page.*/
+        for(var i=0; i<$scope.userData.length;i++){
+                if($scope.currentUser==$scope.userData[i].name){  /*checks each element for a match */
+                    
+                $scope.userData[i].weightLoss = $scope.userData[i].startWeight - weight;
+                    
+                break; /*force end of i-loop search for currentUser*/
+
+                }/*end of if statment*/
+        }/*end of i-loop*/
+        
+        $scope.getWeightLoss();
+
+    }/*end of weighIn function*/
+    /*                          End of Weigh-In Codebase               */    
+
     /*                                        Log-In Codebase               */
     
     /*function attached to log-In page submit button. Takes as parameters the current userName variable (from logIn page) and pwd variable (from logIn page) to insert into logInformation array. This array is sent to the logIn.php.*/
     $scope.logIn = function(userName, pwd) {
+        /*sets teh userName as a global varible currentUser*/
+        $scope.currentUser = userName;
 
         /*This loop searchs the security array. A if statement compare user’s pwd and userName to cred json records. If found to be true, the newArray is return to the controller.*/
         for(var i=0; i<$scope.cred.length;i++){
@@ -53,7 +72,7 @@ app.controller('monsterController', function($scope, $http, $filter){
                 }
 }/*end of for i-loop*/ 
 }/*end of for logIn Function*/ 
-    
+    /*                          End of Log-In Codebase               */    
 
     
 });
